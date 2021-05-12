4aef5595a708335280d208c74e56aec76612c433
Eunheui Kim <dmsgml110712@gmail.com>
2021-05-12 13:52:10 +0900

    FIRST COMMIT

---
+++ Sample/20210512_최초 연습.sql
@@ -0,0 +1,8 @@
+SELECT memberID, memberName 
+  FROM memberTBL;
+ WHERE memberID = 'Eun';
+
+--SELECT productName, cost, company FROM productTBL;
+
+INSERT INTO memberTBL
+VALUES ('Bang', '방문식', '부산시 진구');
