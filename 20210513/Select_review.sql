685b89bef173db7fe08ea01463ad31988c6e11a1
Eunheui Kim <dmsgml110712@gmail.com>
2021-05-13 12:47:57 +0900

    오전 커밋

--- 20210513/Select_review.sql
+++ 20210513/Select_review.sql
@@ -13,6 +13,23 @@ SELECT COUNT(*) FROM userTbl;
 
 -- 키가 180이상이고
 -- 출생년도가 1970년이후에 태어난 사람의
--- 아이디, 이름, 키
+-- 아이디, 이름, 키 조회
 SELECT userID, name, height FROM userTbl
  WHERE height >= 180 AND birthYear >= 1970;
+
+-- 출생년도순(오름차순)으로 정렬해서 조회
+SELECT * FROM userTbl
+ ORDER BY birthYear DESC; -- ASC | DESC
+
+-- SELECT INTO
+-- userTbl_New 테이블이 생성(Pk 키정보는 미생성)
+SELECT * INTO usertbl_New FROM userTbl;
+
+SELECT * FROM usertbl_New;
+
+SELECT userID, name, addr INTO usertbl_Backup FROM userTbl
+ WHERE addr = '서울';
+
+SELECT * FROM userTbl_Backup;
+
+SELECT * FROM userTbl_Backup2;
